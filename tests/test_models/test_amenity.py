cdcb2162f4bb258dad7a1ccf1bf40f794814fc98
mbonikevin <k.mbonimpay@alustudent.com>
2025-05-07 23:53:23 -0700

    pep8

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-\"\"\"Unittests for Amenity.\"\"\"
+"""Unittests for Amenity."""
 
 import unittest
 from models.test_amenity.py import Amenity
 
 
 class TestAmenity(unittest.TestCase):
-    \"\"\"Tests for Amenity class.\"\"\"
+    """Tests for Amenity class."""
 
     def test_dummy(self):
-        \"\"\"A dummy test.\"\"\"
+        """A dummy test."""
         self.assertTrue(True)
